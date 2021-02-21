50ab829983569bb66f218a592224c534084bece1
JimLynchCodes <mrdotjim@gmail.com>
2021-02-21 17:50:08 -0500

    login

--- scraper/src/login/login.ts
+++ scraper/src/login/login.ts
@@ -10,6 +10,8 @@ export async function login(page: Page) {
         const pwInputSelector = '[name="password"]';
         const loginBtnSelector = 'input[type="submit"]';
 
+        console.log(`logging into finviz: ${process.env.FINVIZ_EMAIL.length}, ${process.env.FINVIZ_PW.length} `)
+
         await page.type(emailInputSelector, process.env.FINVIZ_EMAIL)
         await page.type(pwInputSelector, process.env.FINVIZ_PW)
         await page.click(loginBtnSelector)
